64d11c4c07c2867dbfde7e17974070af607bcf5e
MirzaMubashir0713 <mubashirali0713@gmail.com>
2024-10-28 19:30:44 +0530

    operators and basic function

---
+++ Functions.js
@@ -0,0 +1,10 @@
+// functions
+function sum(){
+    var a=10;
+    var b=20;
+    var c=a+b;
+    return c;
+};
+var res=sum();
+console.log(res);
+
---
+++ Operators.js
@@ -0,0 +1,43 @@
+// arthematic , conditional , bitwise , ternary , logical , urinary 
+var x=20;
+var y=10;
+var sum=x+y;
+var sub=x-y;
+var mul=x*y;
+var div=x/y;
+console.log(sum,sub,mul,div);
+
+// conditional 
+var num1=10;
+var num2=5;
+var A=num1>=num2;
+var B=num1<=num2;
+var C=num1<num2;
+var D=num1>num2;
+var E=num1!=num2;
+console.log(A,B,C,D,E);
+// logical 
+var a=4;
+var b=5;
+var c=a&&b;
+var d=a||b;
+console.log(c,d);
+// bitwise
+var e=a&b;
+var f=a|b;
+console.log(e,f);
+// urinary
+var preinc1=++x;
+var preinc2=++y;
+console.log(preinc1);
+console.log(preinc2);
+var predec1=--x;
+var predec2=--y;
+console.log(predec1);
+console.log(predec2);
+
+ 
+
+
+
+
