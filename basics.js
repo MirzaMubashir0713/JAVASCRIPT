3a555db2a4337dd48d788bd0c335c1213ec9568f
MirzaMubashir0713 <mubashirali0713@gmail.com>
2024-10-28 13:06:01 +0530

    basics of javascript

---
+++ basics.js
@@ -0,0 +1,37 @@
+/* data types 
+number , string , boolean , underfined , function , objects , symbol
+*/
+// numbers 
+var num = 20;
+console.log(num);
+
+var x=20;
+var y=30;
+console.log(y,x);
+
+console.log(typeof x);
+console.log(typeof num);
+console.log(typeof y);
+var passed=true;
+console.log(passed);
+console.log(typeof passed);
+
+// data conversion
+// string to number
+var num1="10";
+// var con=parseInt(num1);
+var con1=+num1;
+console.log(typeof con1);
+// number to string
+var num2=10;
+var con2=num2.toString();
+console.log(typeof con2);
+// String to Boolean
+var Str="mubashir";
+var con3=!!Str;
+console.log(typeof con3);
+
+
+
+
+
