cd76f9d35099f7f528d5e68700f4a34ce9ad2626
MirzaMubashir0713 <mubashirali0713@gmail.com>
2024-10-29 15:49:09 +0530

    array basics

---
+++ Arrays.js
@@ -0,0 +1,26 @@
+// Arrays
+var arr=[30,50,60,20,90,85,60];
+// array length
+console.log(arr.length);
+// add at beginning add at end
+arr.unshift(1)
+arr.push(25,40);
+console.log(arr);
+// delete at beginning delete at end
+arr.shift(1);
+arr.pop(40)
+console.log(arr);
+// Searching element in array from startinf and end
+console.log(arr.indexOf(60));
+console.log(arr.lastIndexOf(60));
+// create array from array
+console.log(arr.slice(2,6));
+// deleting from middle
+console.log(arr.splice(2,2));
+console.log(arr);
+// Sorting
+console.log(arr.sort());
+
+
+
+
