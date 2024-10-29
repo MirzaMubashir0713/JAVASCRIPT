904b23fe092785a53f23456c1e5b42773d73a3f2
MirzaMubashir0713 <mubashirali0713@gmail.com>
2024-10-29 14:58:33 +0530

    fucntions basics

--- Functions.js
+++ Functions.js
@@ -8,3 +8,29 @@ function sum(){
 var res=sum();
 console.log(res);
 
+function sub(a,b){
+    var c = a-b;
+    return c;
+}
+console.log(sub(20,5));
+
+// function expression
+
+var exp1=function(){
+    var A=10;
+    var B=5;
+    var add=A+B;
+    console.log(add);
+    return add;
+};
+console.log(exp1());
+
+var exp2=function(x,y){
+    var z=x-y;
+    return z;
+}
+console.log(exp2(20,16));
+
+
+
+
