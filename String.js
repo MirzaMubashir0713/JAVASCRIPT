52a90ed6f7c14fd8e102853ef6fced7f4d759d39
MirzaMubashir0713 <mubashirali0713@gmail.com>
2024-10-29 15:28:43 +0530

    string basics

---
+++ String.js
@@ -0,0 +1,33 @@
+//String
+var str="       My name is mirza mubashir ali baig       ";
+// String length
+console.log(str.length);
+// String Search from starting
+console.log(str.indexOf("ali"));
+// String Search from last
+console.log(str.lastIndexOf("ali"));
+// Sub String 
+console.log(str.substring(17,34));
+// string to lower case
+console.log(str.toLowerCase());
+// String to upper case
+console.log(str.toUpperCase());
+// String trim
+console.log(str.trim());
+console.log(str.trimStart());
+console.log(str.trimEnd());
+// string split
+console.log(str.split(''));
+// replace
+console.log(str.replaceAll("name is","self"));
+// String add at start and end
+console.log(str.padStart(53,"hello"));
+console.log(str.padEnd(51,"bye"));
+
+
+
+
+
+
+
+
